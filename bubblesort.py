7d9460c8e397c831aeca8a50ddc98c1a02778dd6
Nam Tran <namtran05.ca@gmail.com>
2026-05-13 14:37:19 -0400

    bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,3 +1,13 @@
-print("Hello World")
-print("Anh C")
-print("worse code")
+import math
+lst1 = [35,29,65,13,1,205,3]
+
+def bbsort(lst):
+    print(lst)
+    for i in range(len(lst)-1,-1,-1):
+        for j in range(0,i):
+            if lst[j] > lst[j+1]:
+                lst[j],lst[j+1] = lst[j+1],lst[j]
+            print(lst)
+    return lst
+
+bbsort(lst1)
